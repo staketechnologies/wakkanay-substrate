98333e908a4870e2742d59ebe97745b56bf8885b
Shuhei Hiya <syuhei176@gmail.com>
2020-01-14 08:43:38 +0900

    introduce typescript and wakkanay #1

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import {
+  types,
+  ovm,
+  db,
+  verifiers,
+  coder,
+  contract,
+  events,
+  wallet,
+  utils
+} from 'wakkanay'
+
+export { types, ovm, db, verifiers, coder, contract, events, wallet, utils }
